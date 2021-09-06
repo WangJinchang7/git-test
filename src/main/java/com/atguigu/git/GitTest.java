6874f7c55588d1fbd1e3ea42920e91d1a49f4d51
Wjc <Wjc@244.com>
2021-09-06 09:24:02 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+/*
+ *
+ *@author: wjc
+ *@create: 2021/9/6  9:18
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git!");
+    }
+}
